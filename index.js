8c8b43d9f2c7b46ef89e437c1fc8ca281b6cc346
Pein Freecs <plants.coordinators@gmail.com>
2020-06-07 03:01:08 +0800

    first code commit

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-exports.logs = function(){
+var fs = require('fs');
+var path = require('path');
+var dirHidden = '.grasseum';
+var grasseum_core = require("grasseum_core");
+exports.createLogs = function(dir){
 
-    console.log.apply(this,arguments)
+    
+    
+
+    var pathJoin = path.join(dir,dirHidden);
+    if (!fs.existsSync(pathJoin)){
+       try{
+        fs.mkdirSync(pathJoin);
+        grasseum_core.logsColorGreen("Logs folders is now generated");
+       }catch(e){
+        grasseum_core.logsColorRed("Failed generate Logs folders");
+       }
+        
+    }
 }
