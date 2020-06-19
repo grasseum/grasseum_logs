3c421483b0ef730b4ac312f093731043695df0b9
Pein Freecs <plants.coordinators@gmail.com>
2020-06-19 14:37:55 +0800

    initial update in system design

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-var fs = require('fs');
+
 var path = require('path');
 var dirHidden = '.grasseum';
 var grasseum_core = require("grasseum_core");
+var grasseum_directory = require("grasseum_directory");
+var directory_cmd = grasseum_directory.directory();
+const log = grasseum_core.log();
 exports.createLogs = function(dir){
 
     
     
 
     var pathJoin = path.join(dir,dirHidden);
-    if (!fs.existsSync(pathJoin)){
-       try{
-        fs.mkdirSync(pathJoin);
-        grasseum_core.logsColorGreen("Logs folders is now generated");
-       }catch(e){
-        grasseum_core.logsColorRed("Failed generate Logs folders");
+    directory_cmd.createFolder(pathJoin,function(action){
+       if(action["status"] == "folder_created"){
+        log.green("Logs folders is now generated");
        }
         
-    }
+    },function(e){
+        log.red("Failed generate Logs folders");
+    })
 }
